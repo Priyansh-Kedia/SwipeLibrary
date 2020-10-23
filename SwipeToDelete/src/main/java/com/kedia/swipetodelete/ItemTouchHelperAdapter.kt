145491ca27d56c72fa50195841afd1e1965f25c7
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-10-23 18:05:25 +0530

    removed interface methods

--- SwipeToDelete/src/main/java/com/kedia/swipetodelete/ItemTouchHelperAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.kedia.swipetodelete
-
-interface ItemTouchHelperAdapter {
-
-    fun onItemMove(fromPosition: Int, toPosition: Int)
-
-}
--- app/src/main/java/com/kedia/customswipelibrary/Adapter.kt
+++ app/src/main/java/com/kedia/customswipelibrary/Adapter.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.kedia.swipetodelete.ItemTouchHelperAdapter
 
 class Adapter(
     private val context: Context,
